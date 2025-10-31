7168b036a0ff67c987ccc5e87aaaaf58b856cd33
Nails8919 <shorty_love_12@hotmail.com>
2025-10-31 11:00:43 -0400

    deletebutton component update

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,9 +1,11 @@
 
 const DeleteButton = () => {
   return (
-    <button className="rounded-lg cursor-pointer bg-gray-300 h-10 w-10 hover:bg-gray-400" title="Delete task">
-      <i className="fa-solid fa-trash text-2xl rounded-lg pt-1 px-1 pb-1 py-1 cursor-pointer"></i>
-    </button>
+    <>
+      <button className="rounded-lg cursor-pointer bg-gray-300 h-10 w-10 hover:bg-gray-400" title="Delete task">
+        <i className="fa-solid fa-trash text-2xl rounded-lg pt-1 px-1 pb-1 py-1 cursor-pointer"></i>
+      </button>
+    </>
   )
 }
 
